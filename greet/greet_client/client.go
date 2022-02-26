0ab05a431cb787790b66639a81b97ceed938501e
rkritchat <rkritchat@gmail.com>
2022-02-26 23:59:39 +0700

    add Server Streaiming

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"hello-grpc/greet/greetpb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -17,7 +18,9 @@ func main() {
 	}
 	defer cc.Close()
 	c := greetpb.NewGreetServiceClient(cc)
-	doUnary(c)
+	//doUnary(c)
+
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -34,4 +37,30 @@ func doUnary(c greetpb.GreetServiceClient) {
 	}
 
 	fmt.Printf("resp: %v", resp.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Server Streaming RPC...")
+
+	resStream, err := c.GreetManyTimes(context.Background(), &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "rkritchat",
+			LastName:  "rojanaphruk",
+		},
+	})
+	if err != nil {
+		log.Fatalf("err while invoke GreetManyTimes: %v", err)
+	}
+
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			//end of the stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("err while recieve: %v", err)
+		}
+		log.Printf("Response from GreetManyTimes: %v", msg.GetResult())
+	}
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -6,6 +6,8 @@ import (
 	"hello-grpc/greet/greetpb"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -35,4 +37,21 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 	return &greetpb.GreetResponse{
 		Result: result,
 	}, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("Greet Many times function invoke with %v\n", req)
+	firstname := req.GetGreeting().GetFirstName()
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstname + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		err := stream.Send(res)
+		if err != nil {
+			return err
+		}
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
 }
